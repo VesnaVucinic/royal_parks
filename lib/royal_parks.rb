af630246561d693f67e00375c5d3fa6cffec1dbe
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2019-12-21 15:53:50 +0000

    Add files for classes offer, royal_parks, scraper

--- lib/royal_parks.rb
+++ lib/royal_parks.rb
@@ -1,6 +1,7 @@
 require_relative "./royal_parks/version"
 require_relative "./royal_parks/cli"
 
+require "pry"
 module RoyalParks
   class Error < StandardError; end
   # Your code goes here...
--- lib/royal_parks/cli.rb
+++ lib/royal_parks/cli.rb
@@ -2,6 +2,12 @@ class RoyalParks::CLI
 
     def call
         puts "Welcome to the Royal Parks of London!"
+        binding.pry
     end
+
+    def get_royal_parks
+        #to be scraped
+        @parks = RoyalParks::Park.all
+      end
 
 end
