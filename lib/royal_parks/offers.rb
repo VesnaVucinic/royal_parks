0de9faaaa3a1824d0ffb8d58e17ddabd288d0cdd
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2019-12-22 19:26:03 +0000

    Save offer instances

--- lib/royal_parks/offers.rb
+++ lib/royal_parks/offers.rb
@@ -1,25 +1,19 @@
 class RoyalParks::Offer
-    attr_accessor :name, :park, :link
-    @@all = []
-     
-    def initialize(name, park, link)
-      @name = name
-      @park = park
-      @link = link
-      # notify park about the offer
-      add_to_park
-      save
-    end 
-    
-    def self.all 
-      @@all 
-    end
-    
-    def add_to_park
-      @park.offers << self unless @park.offers.include?(self)
-    end 
-    
-    def save
-      @@all << self
-    end
+  attr_accessor :name, :park, :link
+  @@all = []
+   
+  def initialize(name, park, link)
+    @name = name
+    @park = park
+    @link = link
+  save
   end 
+
+  def self.all 
+    @@all 
+  end
+
+  def save
+    @@all << self
+  end
+end 
